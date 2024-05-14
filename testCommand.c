73df7258e2c054c6bf92288d1149463347c8ab71
CyrilleMarc <cyrillemarc1@gmail.com>
2024-05-14 21:53:57 +0200

    commit

---
+++ testCommand.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /* Affichage du contenu du répertoire courant sur un
+       poste équipé d'un système d'exploitation Unix */
+
+    const char *command = "ls -la";
+    int cr = system(command);
+    if (cr != 0)
+    {
+        fprintf(stderr,
+                "Impossible de lancer la commande : %s\n",
+                command);
+    }
+
+    return 0;
+}
